f04e9b81050fd2763a1a2c8ae0b3a1107df03d1d
lene-judika <lene@mailbox.org>
2016-06-24 21:19:58 +0200

    post polls funktioniert. fehler noch nicht abgefangen

--- dbio.py
+++ dbio.py
@@ -13,19 +13,19 @@ def dict_factory(cursor, row):
 
 #*******************************************************************************
 def dbCall(stmt):
+    print('dbCall stmt:')
+    print (stmt)
     conn = sqlite3.connect('polls.db')
     conn.row_factory = dict_factory
     c = conn.cursor()
     c.execute(stmt)
-    print('dbCall')
-    print(c.fetchall())
+    conn.commit()
     return c.fetchall()
 
 #*******************************************************************************
 def stmtNextID(id, table):
     stmt = "select max(" + id + ") as max from " + table
     r = dbCall(stmt)[0]['max']
-    print(r)
     if (r is None):
         r = 1
     return r
@@ -33,16 +33,33 @@ def stmtNextID(id, table):
 #*******************************************************************************
 def postPoll(obj):
     print obj
+    ### generate next poll-ID PID
     pid = stmtNextID('pid', 'polls')
-    print()
+
+    ### insert new poll
     n = obj['name']
-    app = obj['appointments']
     pw = obj['password']
     stmt = "insert into polls values ({0}, '{1}', '{2}')".format(pid, pw, n)
-    print(stmt)
     ret = dbCall(stmt)
-    print(ret)
-    aid = stmtNextID('aid', 'appointments')
+
+    ### insert new appointments
+    app = obj['appointments']
     for date in app:
+        aid = stmtNextID('aid', 'appointments')
         stmt = "insert into appointments values({0}, {1}, '{2}')".format(aid, pid, date)
         dbCall(stmt)
+
+    ### create return-data
+    stmt = "select p.PID, p.name, a.aid, a.date \
+            from polls p, appointments a\
+            where p.pid={0} and a.pid={1}".format(pid,pid)
+    r = dbCall(stmt)
+    applist = []
+    retdict = {}
+    for row in r:
+        applist.append(row['date'])
+    retdict['pid'] = r[0]['pid']
+    retdict['name'] = r[0]['name']
+    retdict['appointments'] = applist
+
+    return retdict
