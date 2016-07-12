040ac797cc25746ad3e4a51c7a49d0f0f9f848ab
Stefan Nitz <nitz.stefan@googlemail.com>
2016-07-12 21:12:32 +0200

    new component model

--- client/src/CRUDApp.hs
+++ client/src/CRUDApp.hs
@@ -1,14 +1,7 @@
 {-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, TypeFamilies, UndecidableInstances #-}
 
 module CRUDApp
-  ( Model
-  , Msg(..)
-  , initModel
-  , update
-  , view
-  , Info(Info)
-  , Actions(Actions)
-  , ChangeAction(..)
+  ( crudComponent
   ) where
 
   import Widget
@@ -24,31 +17,32 @@ module CRUDApp
 
 
 
-  data ChangeAction = Update | Create
+  data ChangeAction = ActionUpdate | ActionCreate
     deriving (Eq)
 
   data Mode sm cm = ModeOverview | ModeRead sm | ModeChange ChangeAction cm
   makePrisms ''Mode
 
-  data Actions a i = Actions
-    { _createCmd :: a -> Cmd (Either String a)
-    , _readCmd   :: i -> Cmd (Either String a)
-    , _updateCmd :: a -> Cmd (Either String a)
-    , _deleteCmd :: a -> Cmd (Either String ())
+  data Actions m a i = Actions
+    { _createCmd :: a -> m (Either String a)
+    , _readCmd   :: i -> m (Either String a)
+    , _updateCmd :: a -> m (Either String a)
+    , _deleteCmd :: a -> m (Either String ())
     }
   makeLenses ''Actions
 
   data Msg dmsg smsg cmsg a i =
       SetCursor a
-    | Recv a
+    | Insert a
     | Remove i
     | ClickCreate
     | ClickRead
     | ClickUpdate
     | ClickDelete
-    | Send a
-    | Read i
-    | Delete a
+    | CreateCmd a
+    | ReadCmd i
+    | UpdateCmd a
+    | DeleteCmd a
     | Back
     | Ignore
     | DisplayMsg dmsg
@@ -56,81 +50,91 @@ module CRUDApp
     | ChangerMsg cmsg
     | Error String
 
-  data Info r a i = Info
-    { _infoIdentifier  :: a -> i
-    , _infoNew         :: a
-    , _infoDef         :: Maybe i
-    , _infoActions     :: Actions a i
-    , _infoName        :: a -> String
-    , _infoDisplayInit :: r
+  data DisplayEvent i = DEChanged i | DEClose
+  data SelectorEvent i = SESelect i | SEClose
+  data ChangerEvent a = CECreate a | CEUpdate a | CEClose
+
+  data Info m dm dmsg sm smsg cm cmsg a i = Info
+    { _infoIdentifier :: a -> i
+    , _infoNew        :: a
+    , _infoDef        :: Maybe i
+    , _infoActions    :: Actions m a i
+    , _infoName       :: a -> String
+    , _infoDisplay    :: Component m a                 dm dmsg (DisplayEvent i)
+    , _infoSelector   :: Component m (Maybe i)         sm smsg (SelectorEvent i)
+    , _infoChanger    :: Component m (a, ChangeAction) cm cmsg (ChangerEvent a)
     }
   makeFields ''Info
 
-  data Model dm sm cm r a i = Model
-    { _modelInfo    :: Info r a i
-    , _modelValues  :: Map i a
-    , _modelDisplay :: Maybe (i, dm)
-    , _modelMode    :: Mode sm cm
+  data Model m dm dmsg sm smsg cm cmsg a i = Model
+    { _modelInfo      :: Info m dm dmsg sm smsg cm cmsg a i
+    , _modelValues    :: Map i a
+    , _modelDisplayed :: Maybe (i, dm)
+    , _modelMode      :: Mode sm cm
     }
   makeFields ''Model
 
-  selected :: Ord i => Model dm sm cm r a i -> Maybe a
+  data Event a i = Create a | Read i | Update a | Delete i
+
+  displayEvent :: DisplayEvent i -> Msg dmsg smsg cmsg a i
+  displayEvent (DEChanged i) = ReadCmd i
+  displayEvent DEClose = Back
+
+  selectorEvent :: SelectorEvent i -> Msg dmsg smsg cmsg a i
+  selectorEvent (SESelect i) = ReadCmd i
+  selectorEvent SEClose = Back
+
+  changerEvent :: ChangerEvent a -> Msg dmsg smsg cmsg a i
+  changerEvent (CECreate a) = CreateCmd a
+  changerEvent (CEUpdate a) = UpdateCmd a
+  changerEvent CEClose = Back
+
+
+  selected :: Ord i => Model m dm dmsg sm smsg cm cmsg a i -> Maybe a
   selected m = do
-    i <- m^?display._Just._1
+    i <- m^?displayed._Just._1
     m^?values.ix i
 
-  selectCmd :: Model dm sm cm r a i -> a -> Cmd (Either String a)
-  selectCmd m = case m^?mode._ModeChange._1 of
-    Just Create -> m^.info.actions.createCmd
-    Just Update -> m^.info.actions.updateCmd
+  crudComponent :: (Applicative m, Monoid (m (Msg dmsg smsg cmsg a i)), Ord i) => Component m (Info m dm dmsg sm smsg cm cmsg a i) (Model m dm dmsg sm smsg cm cmsg a i) (Msg dmsg smsg cmsg a i) (Event a i)
+  crudComponent = Component crudInitModel crudUpdate crudView
 
-  instance
-    ( Component dm
-    , Component sm
-    , Component cm
-    , Init dm ~ (a,r)
-    , Init cm ~ (a, ChangeAction)
-    , Init sm ~ (Maybe i)
-    , Send dm ~ Recv dm
-    , Send sm ~ Msg (Recv dm) (Recv sm) (Recv cm) a i
-    , Send cm ~ Msg (Recv dm) (Recv sm) (Recv cm) a i
-    , Ord i
-    ) => Component (Model dm sm cm r a i) where
-    type Recv   (Model dm sm cm r a i) = Msg (Recv dm) (Recv sm) (Recv cm) a i
-    type Send   (Model dm sm cm r a i) = Msg (Recv dm) (Recv sm) (Recv cm) a i
-    type Init   (Model dm sm cm r a i) = Info r a i
-    initModel i = (Model i Map.empty Nothing ModeOverview, mempty)
+  crudInitModel :: Info m dm dmsg sm smsg cm cmsg a i -> Model m dm dmsg sm smsg cm cmsg a i
+  crudInitModel info = Model info Map.empty Nothing ModeOverview
 
-    update m Ignore            = (m, mempty)
-    update m Back              = ((m & mode .~ ModeOverview) & display .~ Nothing, mempty)
-    update m (SetCursor a)     = let (m', c) = initModel (a, m^.info.displayInit) in (m & display ?~ ((m^.info.identifier) a, m'), fmap DisplayMsg c)
-    update m (Recv a)          = (m & values %~ Map.insert (a^.to (m^.info.identifier)) a, mempty)
-    update m (Remove i)        = (m & values %~ Map.delete i, mempty)
-    update m ClickCreate       = let (m', c) = initModel (m^.info.new, Create) in (m & mode .~ ModeChange Create m', c)
-    update m ClickRead         = let (m', c) = initModel (m^.info.def) in (m & mode .~ ModeRead m', c)
-    update m ClickUpdate       = maybe (m, mempty) (\a -> let (m',c) = initModel (a, Update) in (m & mode .~ ModeChange Update m', c)) (selected m)
-    update m ClickDelete       = maybe (m, mempty) (\i -> (m, cmd . return . Delete $ i)) (selected m)
-    update m (Send a)          = (m, fmap (either Error Recv) (selectCmd m a)               `mappend` cmd (return  Back))
-    update m (Read i)          = (m, fmap (either Error Recv) (m^.info.actions.readCmd $ i) `mappend` cmd (return  Back))
-    update m (Delete a)        = (m, fmap (either Error (const $ Remove ((m^.info.identifier) a))) (m^.info.actions.deleteCmd $ a))
-    update m (SelectorMsg msg) = maybe (m, mempty) (first (\m' -> m & mode._ModeRead .~ m'))                 (update <$> (m^?mode._ModeRead)      <*> pure msg)
-    update m (ChangerMsg msg)  = maybe (m, mempty) (first (\m' -> m & mode._ModeChange._2 .~ m'))            (update <$> (m^?mode._ModeChange._2) <*> pure msg)
-    update m (DisplayMsg msg)  = maybe (m, mempty) ((\m' -> m & display._Just._2 .~ m') *** fmap DisplayMsg) (update <$> (m^?display._Just._2)    <*> pure msg)
-    update m (Error s)         = (m, cmd (putStrLn s >> return Ignore)) --TODO better error handling
+  crudUpdate :: (Applicative m, Monoid (m (Msg dmsg smsg cmsg a i)), Ord i) =>
+    Model m dm dmsg sm smsg cm cmsg a i -> Msg dmsg smsg cmsg a i -> Action m (Model m dm dmsg sm smsg cm cmsg a i) (Msg dmsg smsg cmsg a i) (Event a i)
+  crudUpdate m Ignore            = ActionIgnore
+  crudUpdate m Back              = ActionModel $ m & mode      .~ ModeOverview & displayed .~ Nothing
+  crudUpdate m (Insert a)        = ActionModel $ m & values    %~ Map.insert ((m^.info.identifier) a) a
+  crudUpdate m (Remove i)        = ActionModel $ m & values    %~ Map.delete i
+  crudUpdate m (SetCursor a)     = ActionModel $ m & displayed ?~ ((m^.info.identifier) a, (m^.info.display.initModel) a)
+  crudUpdate m ClickCreate       = ActionModel $ m & mode      .~ ModeChange ActionCreate ((m^.info.changer.initModel) (m^.info.new, ActionCreate))
+  crudUpdate m ClickRead         = ActionModel $ m & mode      .~ ModeRead ((m^.info.selector.initModel) (m^.info.def))
+  crudUpdate m ClickUpdate       = maybe ActionIgnore (\a -> ActionModel $ m & mode .~ ModeChange ActionUpdate ((m^.info.changer.initModel) (a, ActionUpdate))) (selected m)
+  crudUpdate m ClickDelete       = maybe ActionIgnore (ActionMsg . DeleteCmd) (selected m)
+  crudUpdate m (CreateCmd a)     = ActionCmd $ fmap (either Error Insert) (m^.info.actions.createCmd $ a) `mappend` pure Back
+  crudUpdate m (ReadCmd i)       = ActionCmd $ fmap (either Error Insert) (m^.info.actions.readCmd $ i)   `mappend` pure Back
+  crudUpdate m (UpdateCmd a)     = ActionCmd $ fmap (either Error Insert) (m^.info.actions.updateCmd $ a) `mappend` pure Back
+  crudUpdate m (DeleteCmd a)     = ActionCmd $ fmap (either Error (const $ Remove . (m^.info.identifier) $ a)) (m^.info.actions.deleteCmd $ a)
+  crudUpdate m (DisplayMsg msg)  = maybe ActionIgnore (\dm -> component dm msg (\dm' -> m & displayed._Just._2  .~ dm') DisplayMsg  displayEvent  (m^.info.display.update))  (m^?displayed._Just._2)
+  crudUpdate m (SelectorMsg msg) = maybe ActionIgnore (\sm -> component sm msg (\sm' -> m & mode._ModeRead      .~ sm') SelectorMsg selectorEvent (m^.info.selector.update)) (m^?mode._ModeRead)
+  crudUpdate m (ChangerMsg msg)  = maybe ActionIgnore (\cm -> component cm msg (\cm' -> m & mode._ModeChange._2 .~ cm') ChangerMsg  changerEvent  (m^.info.changer.update))  (m^?mode._ModeChange._2)
+  crudUpdate m (Error s)         = ActionIgnore
 
-    view (Model i vs c ModeOverview) = mkBox Horizontal $
-      [ mkScrolledWindow $ mkTextList (Map.elems vs) (i^.name) (Just SetCursor) --TODO highlight cursor
-      , mkBox Vertical
-        [ mkButton "Create" (Just ClickCreate)
-        , mkButton "Read"   (Just ClickRead)
-        , mkButton "Update" (Just ClickUpdate)
-        , mkButton "Delete" (Just ClickDelete)
-        ]
-      ] ++ case c of
-        Nothing ->
-          []
-        Just (_, dm) ->
-          [DisplayMsg <$> view dm]
+  crudView :: Model m dm dmsg sm smsg cm cmsg a i -> Widget (Msg dmsg smsg cmsg a i)
+  crudView (Model i vs c ModeOverview) = mkBox Horizontal $
+    [ mkScrolledWindow $ mkTextList (Map.elems vs) (i^.name) (Just SetCursor) --TODO highlight cursor
+    , mkBox Vertical
+      [ mkButton "Create" (Just ClickCreate)
+      , mkButton "Read"   (Just ClickRead)
+      , mkButton "Update" (Just ClickUpdate)
+      , mkButton "Delete" (Just ClickDelete)
+      ]
+    ] ++ case c of
+      Nothing ->
+        []
+      Just (_, dm) ->
+        [DisplayMsg <$> (i^.display.view) dm]
 
-    view (Model _ _ _ (ModeRead sm))     = view sm
-    view (Model _ _ _ (ModeChange c cm)) = view cm
+  crudView (Model i _ _ (ModeRead sm))     = SelectorMsg <$> (i^.selector.view) sm
+  crudView (Model i _ _ (ModeChange c cm)) = ChangerMsg  <$> (i^.changer.view) cm
