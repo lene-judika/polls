908431ea3bdac9ffc9c2575136f11802afb8b11e
Stefan Nitz <nitz.stefan@googlemail.com>
2016-07-13 22:22:47 +0200

    added token

--- client2/src/Network.hs
+++ client2/src/Network.hs
@@ -14,15 +14,15 @@ module Network (send) where
 
   liftExceptT = ExceptT . return
 
-  send :: String -> RequestMethod -> String -> (String -> Either String a) ->  ExceptT String IO a
-  send h r body f = do
+  send :: String -> RequestMethod -> Maybe String -> String -> (String -> Either String a) ->  ExceptT String IO a
+  send h r token body f = do
     uri <- ExceptT . return . maybe (Left $ "ParseError: Not a valid URI: " ++ h) Right . parseURI $ h
-    let req = setRequestBody (mkRequest r uri) ("application/json", body)
-    liftIO . putStrLn . ("request: " ++) . show $ req
-    liftIO . putStrLn . rqBody $ req
+    let req = maybe id (\t -> insertHeader HdrAuthorization ("Token token=" ++ t)) token $ setRequestBody (mkRequest r uri) ("application/json", body)
+    liftIO . putStrLn . ("DEBUG: request: " ++) . show $ req
+    liftIO . putStrLn . ("DEBUG: " ++) . rqBody $ req
     response <- join . fmap (withExceptT show . liftExceptT) . withExceptT (show :: IOException -> String) . ExceptT . try . simpleHTTP $ req -- TODO replace show, for better error messages
-    liftIO . putStrLn . ("response: " ++) . show $ response
-    liftIO . putStrLn . rspBody $ response
+    liftIO . putStrLn . ("DEBUG: response: " ++) . show $ response
+    liftIO . putStrLn . ("DEBUG: " ++) . rspBody $ response
     case rspCode response of
       (2,_,_) ->
         liftExceptT . f . rspBody $ response
